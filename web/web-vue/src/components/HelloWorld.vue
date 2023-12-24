11af3c1392455dfad9dd1c50fa33352042b952cb
Eleven <idiotalex@163.com>
2023-12-24 14:53:37 +0800

    init web-vue

--- web/web-vue/src/components/HelloWorld.vue
+++ web/web-vue/src/components/HelloWorld.vue
@@ -1,36 +1,72 @@
 <script setup>
 import { ref } from 'vue'
+import Dock from 'primevue/dock';
+
 
 defineProps({
   msg: String,
 })
 
 const count = ref(0)
+const items = ref([
+    {
+        label: 'Finder',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/finder.svg",
+        command: () => {
+            displayFinder.value = true;
+        }
+    },
+    {
+        label: 'Terminal',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/terminal.svg",
+        command: () => {
+            displayTerminal.value = true;
+        }
+    },
+    {
+        label: 'App Store',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/appstore.svg",
+        command: () => {
+            toast.add({ severity: 'error', summary: 'An unexpected error occurred while signing in.', detail: 'UNTRUSTED_CERT_TITLE', group: 'tc', life: 3000 });
+        }
+    },
+    {
+        label: 'Safari',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/safari.svg",
+        command: () => {
+            toast.add({ severity: 'warn', summary: 'Safari has stopped working', group: 'tc', life: 3000 });
+        }
+    },
+    {
+        label: 'Photos',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/photos.svg",
+        command: () => {
+            displayPhotos.value = true;
+        }
+    },
+    {
+        label: 'GitHub',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/github.svg",
+    },
+    {
+        label: 'Trash',
+        icon: "https://primefaces.org/cdn/primevue//images/dock/trash.png",
+        command: () => {
+            toast.add({ severity: 'info', summary: 'Empty Trash', life: 3000 });
+        }
+    }
+]);
 </script>
 
 <template>
+  <Dock :model="items">
+    <template #item="{ item }">
+        <a v-tooltip.top="item.label" href="#" class="p-dock-link" @click="onDockItemClick($event, item)">
+            <img :alt="item.label" :src="item.icon" style="width: 100%" />
+        </a>
+    </template>
+  </Dock>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
---
+++ web/web-vue/src/layout/AppMenu.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: '主页',
+        items: [{ label: '仪表盘', icon: 'pi pi-fw pi-home', to: '/' }]
+    },
+    {
+        label: '项目',
+        items: [
+            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
+            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
+            { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
+            { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
+            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
+        ]
+    },
+    {
+        label: '构建',
+        items: [
+            { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: 'NEW' },
+            { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: 'https://www.primefaces.org/primeblocks-vue', target: '_blank' }
+        ]
+    },
+    {
+        label: '环境',
+        items: [
+            { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },
+            { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
+        ]
+    },
+    {
+        label: 'Pages',
+        icon: 'pi pi-fw pi-briefcase',
+        to: '/pages',
+        items: [
+            {
+                label: 'Landing',
+                icon: 'pi pi-fw pi-globe',
+                to: '/landing'
+            },
+            {
+                label: 'Auth',
+                icon: 'pi pi-fw pi-user',
+                items: [
+                    {
+                        label: 'Login',
+                        icon: 'pi pi-fw pi-sign-in',
+                        to: '/auth/login'
+                    },
+                    {
+                        label: 'Error',
+                        icon: 'pi pi-fw pi-times-circle',
+                        to: '/auth/error'
+                    }
+                ]
+            },
+            {
+                label: 'Crud',
+                icon: 'pi pi-fw pi-pencil',
+                to: '/pages/crud'
+            },
+            {
+                label: 'Timeline',
+                icon: 'pi pi-fw pi-calendar',
+                to: '/pages/timeline'
+            }
+        ]
+    },
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
